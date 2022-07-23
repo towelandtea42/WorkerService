85553c26081692b7d7b62c3ab32534ed084a2704
RodrigoFNascimento <rodrigofn0@hotmail.com>
2022-07-23 11:16:27 -0300

    Initial commit

---
+++ src/WorkerService/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Logging.EventLog;
+using WorkerService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        LoggerProviderOptions.RegisterProviderOptions<
+            EventLogSettings, EventLogLoggerProvider>(services);
+
+        services.AddHostedService<Worker>();
+    })
+    .ConfigureLogging((context, logging) =>
+    {
+        // See: https://github.com/dotnet/runtime/issues/47303
+        logging.AddConfiguration(
+            context.Configuration.GetSection("Logging"));
+    })
+    .Build();
+
+await host.RunAsync();
---
+++ src/WorkerService/Worker.cs
@@ -0,0 +1,37 @@
+namespace WorkerService;
+
+public class Worker : BackgroundService
+{
+    private readonly ILogger<Worker> _logger;
+
+    public Worker(ILogger<Worker> logger)
+    {
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        try
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "{Message}", ex.Message);
+
+            // Terminates this process and returns an exit code to the operating system.
+            // This is required to avoid the 'BackgroundServiceExceptionBehavior', which
+            // performs one of two scenarios:
+            // 1. When set to "Ignore": will do nothing at all, errors cause zombie services.
+            // 2. When set to "StopHost": will cleanly stop the host, and log errors.
+            //
+            // In order for the Windows Service Management system to leverage configured
+            // recovery options, we need to terminate the process with a non-zero exit code.
+            Environment.Exit(1);
+        }
+    }
+}
